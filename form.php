5b350225a5d4456a8fc99f708973f8d59a2fc2bd
Román Cinalli <romancinalli36@gmail.com>
2022-04-18 22:49:37 -0300

    2da pre entrega

---
+++ form.php
@@ -0,0 +1,11 @@
+<?php
+
+$mail=$_POST['Email'];
+$mensaje=$_POST['textarea'];
+
+$para='romancinalli@hotmail.com';
+$asunto='Este mail fue enviado desde la web';
+
+mail($para, $asunto, utf8_decode($mensaje));
+
+header('location:index.html');
